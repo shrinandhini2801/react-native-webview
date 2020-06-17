a3db967ebb852277ffefbabf484c6149c6dc71fb
shrinandhinijr <snandhini@torstar.ca>
2020-06-17 16:43:28 -0400

    Custom webview implementation

--- src/WebView.android.tsx
+++ src/WebView.android.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { v4 as uuid } from 'uuid';
 
 import {
   Image,
@@ -43,6 +42,11 @@ const RNCWebView = requireNativeComponent(
 const { resolveAssetSource } = Image;
 
 /**
+ * A simple counter to uniquely identify WebView instances. Do not use this for anything else.
+ */
+let uniqueRef = 0;
+
+/**
  * Renders a native WebView.
  */
 class WebView extends React.Component<AndroidWebViewProps, State> {
@@ -71,15 +75,13 @@ class WebView extends React.Component<AndroidWebViewProps, State> {
     lastErrorEvent: null,
   };
 
-  uniqueRef = uuid().replace(/-/g, '');
-
   webViewRef = React.createRef<NativeWebViewAndroid>();
 
-  componentDidMount = () => {
-    BatchedBridge.registerCallableModule(this.getMessagingModuleName(), this);
-  }
+  messagingModuleName = `WebViewMessageHandler${(uniqueRef += 1)}`;
 
-  getMessagingModuleName = () => `WebViewMessageHandler${this.uniqueRef}`;
+  componentDidMount = () => {
+    BatchedBridge.registerCallableModule(this.messagingModuleName, this);
+  };
 
   getCommands = () => UIManager.getViewManagerConfig('RNCWebView').Commands;
 
@@ -87,7 +89,7 @@ class WebView extends React.Component<AndroidWebViewProps, State> {
     UIManager.dispatchViewManagerCommand(
       this.getWebViewHandle(),
       this.getCommands().goForward,
-      undefined
+      undefined,
     );
   };
 
@@ -95,7 +97,7 @@ class WebView extends React.Component<AndroidWebViewProps, State> {
     UIManager.dispatchViewManagerCommand(
       this.getWebViewHandle(),
       this.getCommands().goBack,
-      undefined
+      undefined,
     );
   };
 
@@ -106,7 +108,7 @@ class WebView extends React.Component<AndroidWebViewProps, State> {
     UIManager.dispatchViewManagerCommand(
       this.getWebViewHandle(),
       this.getCommands().reload,
-      undefined
+      undefined,
     );
   };
 
@@ -114,7 +116,7 @@ class WebView extends React.Component<AndroidWebViewProps, State> {
     UIManager.dispatchViewManagerCommand(
       this.getWebViewHandle(),
       this.getCommands().stopLoading,
-      undefined
+      undefined,
     );
   };
 
@@ -122,7 +124,7 @@ class WebView extends React.Component<AndroidWebViewProps, State> {
     UIManager.dispatchViewManagerCommand(
       this.getWebViewHandle(),
       this.getCommands().requestFocus,
-      undefined
+      undefined,
     );
   };
 
@@ -136,25 +138,25 @@ class WebView extends React.Component<AndroidWebViewProps, State> {
 
   clearFormData = () => {
     UIManager.dispatchViewManagerCommand(
-       this.getWebViewHandle(),
-       this.getCommands().clearFormData,
-        undefined,
+      this.getWebViewHandle(),
+      this.getCommands().clearFormData,
+      undefined,
     );
-  }
+  };
 
   clearCache = (includeDiskFiles: boolean) => {
     UIManager.dispatchViewManagerCommand(
-       this.getWebViewHandle(),
-       this.getCommands().clearCache,
-       [includeDiskFiles],
+      this.getWebViewHandle(),
+      this.getCommands().clearCache,
+      [includeDiskFiles],
     );
   };
 
   clearHistory = () => {
     UIManager.dispatchViewManagerCommand(
-       this.getWebViewHandle(),
-       this.getCommands().clearHistory,
-        undefined,
+      this.getWebViewHandle(),
+      this.getCommands().clearHistory,
+      undefined,
     );
   };
 
@@ -193,7 +195,9 @@ class WebView extends React.Component<AndroidWebViewProps, State> {
 
   onLoadingStart = (event: WebViewNavigationEvent) => {
     const { onLoadStart } = this.props;
-    const { nativeEvent: { url } } = event;
+    const {
+      nativeEvent: { url },
+    } = event;
     this.startUrl = url;
     if (onLoadStart) {
       onLoadStart(event);
@@ -223,11 +227,13 @@ class WebView extends React.Component<AndroidWebViewProps, State> {
     if (onHttpError) {
       onHttpError(event);
     }
-  }
+  };
 
   onLoadingFinish = (event: WebViewNavigationEvent) => {
     const { onLoad, onLoadEnd } = this.props;
-    const { nativeEvent: { url } } = event;
+    const {
+      nativeEvent: { url },
+    } = event;
     if (onLoad) {
       onLoad(event);
     }
@@ -251,7 +257,9 @@ class WebView extends React.Component<AndroidWebViewProps, State> {
 
   onLoadingProgress = (event: WebViewProgressEvent) => {
     const { onLoadProgress } = this.props;
-    const { nativeEvent: { progress } } = event;
+    const {
+      nativeEvent: { progress },
+    } = event;
     if (progress === 1) {
       this.setState((state) => {
         if (state.viewState === 'LOADING') {
@@ -313,7 +321,7 @@ class WebView extends React.Component<AndroidWebViewProps, State> {
     const webViewStyles = [styles.container, styles.webView, style];
     const webViewContainerStyle = [styles.container, containerStyle];
 
-    if (typeof source !== "number" && source && 'method' in source) {
+    if (typeof source !== 'number' && source && 'method' in source) {
       if (source.method === 'POST' && source.headers) {
         console.warn(
           'WebView: `source.headers` is not supported when using POST.',
@@ -323,8 +331,8 @@ class WebView extends React.Component<AndroidWebViewProps, State> {
       }
     }
 
-    const NativeWebView
-      = (nativeConfig.component as typeof NativeWebViewAndroid) || RNCWebView;
+    const NativeWebView =
+      (nativeConfig.component as typeof NativeWebViewAndroid) || RNCWebView;
 
     const onShouldStartLoadWithRequest = createOnShouldStartLoadWithRequest(
       this.onShouldStartLoadWithRequestCallback,
@@ -338,7 +346,7 @@ class WebView extends React.Component<AndroidWebViewProps, State> {
         key="webViewKey"
         {...otherProps}
         messagingEnabled={typeof onMessage === 'function'}
-        messagingModuleName={this.getMessagingModuleName()}
+        messagingModuleName={this.messagingModuleName}
         onLoadingError={this.onLoadingError}
         onLoadingFinish={this.onLoadingFinish}
         onLoadingProgress={this.onLoadingProgress}
